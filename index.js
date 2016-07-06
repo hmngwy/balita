0e1e4c50dc24827c2acb9b8bc93ee657980f1b8f
hmngwy <cp.ambrosio@gmail.com>
2016-07-06 16:49:26 +0800

    Pagination update

--- index.js
+++ index.js
@@ -15,41 +15,74 @@ var ARTICLE_INDENT = "    ";
 
 var url = 'https://news.google.com.ph/news?cf=all&hl=en&pz=1&ned=en_ph&topic=n&output=rss';
 
+var content;
+
 var readArticle = function readArticle( url ) {
-  read( url, function(err, article, meta) {
+  return new Promise(function(resolve, reject) {
+    read( url, function(err, article, meta) {
 
-    console.log( "\n" + ARTICLE_INDENT + article.title.toUpperCase().trim(), "\n" );
+      content = "\n" + ARTICLE_INDENT + article.title.toUpperCase().trim();
 
-    // make sure we have linebreaks on paragraphs, in case html minified
-    var content = article.content.replace( /\<\/p\>/g, "\n" );
+      // make sure we have linebreaks on paragraphs, in case html minified
+      content = content + article.content.replace( /\<\/p\>/g, "\n" );
 
-    // strip unwanted tags
-    content = sanitize( content, { allowedTags: [ ] } );
+      article.close();
 
-    // hard wrap
-    content = wrap( content );
+      // strip unwanted tags
+      content = sanitize( content, { allowedTags: [ ] } );
 
+      // hard wrap
+      content = wrap( content );
 
-    // condense whitespace, explode by paragraph
-    content = content.split( '\n' ); //.filter( function( str ) { return /\S/.test( str ); });
+      // condense whitespace, explode by paragraph
+      content = content.split( '\n' ); //.filter( function( str ) { return /\S/.test( str ); });
 
-    // trim lines
-    content = _.map(content, function( i ){ return i.trim(); });
+      // trim lines
+      content = _.map(content, function( i ){ return i.trim(); });
 
-    // implode, double linebreak to visually separate paragraphs
-    content = content.join( "\n" );
-    content = content.replace(/\n{3,}/g, '\n\n');
-    content = content.replace(/\n/g, '\n'+ARTICLE_INDENT);
+      // implode, double linebreak to visually separate paragraphs
+      content = ARTICLE_INDENT + content.join( "\n" );
+      content = content.replace(/\n{3,}/g, '\n\n');
+      content = content.replace(/\n/g, '\n'+ARTICLE_INDENT);
 
-    // decode html entities
-    content = entities.decode( content );
-    console.log( content );
+      // decode html entities
+      content = entities.decode( content );
+      content = content.split('\n');
+      content.push('END');
 
-    article.close();
+      resolve(content);
+
+    });
   });
 }
 // readArticle('http://www.philstar.com/headlines/2015/02/28/1428494/french-star-takes-different-role');
 
+function more(content, page) {
+
+  process.stdout.write('\n');
+  inquirer.prompt([
+    {
+      type: 'confirm',
+      message: 'More ['+page+' of '+Math.ceil(content.length/size.height)+']',
+      name: 'continue'
+    }
+  ], function(answer){
+    process.stdout.write('\n');
+    if(answer.continue) {
+      process.stdout.cursorTo(0);
+      process.stdout.clearLine();
+      start = page * size.height-1;
+      _.each(content.slice(start, (start + size.height)-2), function(line){
+        if(line=='END') process.exit();
+        process.stdout.write(line+'\n');
+      });
+      more(content, page++);
+    } else {
+      process.exit();
+    }
+  });
+
+}
 
 http.get(url, function(res) {
   var body = '';
@@ -85,7 +118,12 @@ http.get(url, function(res) {
 
         console.log( "  Loading: "+answers.link );
 
-        readArticle(answers.link);
+        readArticle(answers.link).then(function(content) {
+          _.each(content.slice(0, size.height-2), function(line){
+            process.stdout.write(line+'\n');
+          });
+          more(content, 1);
+        });
 
       });
     });
