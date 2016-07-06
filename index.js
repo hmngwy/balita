366acb428250342b7e92d989fa5e391d38bd68ee
hmngwy <cp.ambrosio@gmail.com>
2016-07-06 17:26:56 +0800

    Fixed pagination, loops back to list

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-#! /usr/bin/env node
+#!/usr/bin/env node
 
 var read = require('node-readability'),
   sanitize = require('sanitize-html'),
@@ -58,13 +58,14 @@ var readArticle = function readArticle( url ) {
 // readArticle('http://www.philstar.com/headlines/2015/02/28/1428494/french-star-takes-different-role');
 
 function more(content, page) {
-
+  var end = false;
   process.stdout.write('\n');
   inquirer.prompt([
     {
       type: 'confirm',
       message: 'More ['+page+' of '+Math.ceil(content.length/size.height)+']',
-      name: 'continue'
+      name: 'continue',
+      default: true
     }
   ], function(answer){
     process.stdout.write('\n');
@@ -73,10 +74,25 @@ function more(content, page) {
       process.stdout.clearLine();
       start = page * size.height-2;
       _.each(content.slice(start, (start + size.height-2)), function(line){
-        if(line=='END') process.exit();
-        process.stdout.write(line+'\n');
+        if(line=='END') {
+          console.log('\r\n');
+          inquirer.prompt([
+            {
+              type: 'confirm',
+              message: 'Back to list?',
+              name: 'read',
+              default: true
+            }
+          ], function(answer){
+            if(answer.read) begin();
+            else process.exit();
+          });
+          end = true;
+        } else {
+          process.stdout.write(line+'\n');
+        }
       });
-      more(content, page+1);
+      if(!end) more(content, page+1);
     } else {
       process.exit();
     }
@@ -84,50 +100,54 @@ function more(content, page) {
 
 }
 
-http.get(url, function(res) {
-  var body = '';
+function begin() {
+  http.get(url, function(res) {
+    var body = '';
 
-  res.on('data', function(chunk) {
-    body += chunk;
-  });
+    res.on('data', function(chunk) {
+      body += chunk;
+    });
 
-  res.on('end', function() {
-    parser(body, function(err, json){
+    res.on('end', function() {
+      parser(body, function(err, json){
 
-      var urlmap = {};
+        var urlmap = {};
 
-      var choices = _.map(json.rss.channel[0].item, function(i){
+        var choices = _.map(json.rss.channel[0].item, function(i){
 
-        var name = i.title.toString();
-        var link = i.link.toString();
-        return {
-          name: entities.decode( name.trim() ),
-          value: link.substring(link.indexOf("url=")+4, link.length)
-        }
-      });
-      choices.push(new inquirer.Separator());
-      inquirer.prompt([
-        {
-          type      : "list",
-          name      : "link",
-          message   : "Balita",
-          paginated : true,
-          choices   : choices
-        }
-      ], function( answers ) {
-
-        console.log( "  Loading: "+answers.link );
-
-        readArticle(answers.link).then(function(content) {
-          _.each(content.slice(0, size.height-2), function(line){
-            process.stdout.write(line+'\n');
-          });
-          more(content, 1);
+          var name = i.title.toString();
+          var link = i.link.toString();
+          return {
+            name: entities.decode( name.trim() ),
+            value: link.substring(link.indexOf("url=")+4, link.length)
+          }
         });
+        choices.push(new inquirer.Separator());
+        inquirer.prompt([
+          {
+            type      : "list",
+            name      : "link",
+            message   : "Balita",
+            paginated : true,
+            choices   : choices
+          }
+        ], function( answers ) {
 
+          console.log( "  Loading: "+answers.link );
+
+          readArticle(answers.link).then(function(content) {
+            _.each(content.slice(0, size.height-2), function(line){
+              process.stdout.write(line+'\n');
+            });
+            more(content, 1);
+          });
+
+        });
       });
     });
+  }).on('error', function(e) {
+    console.log("Got error: ", e);
   });
-}).on('error', function(e) {
-  console.log("Got error: ", e);
-});
+}
+
+begin();
